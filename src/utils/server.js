2bce65b772d120440d84b842e9eacbf4587dd971
CosmicAnon <wescosmic@gmail.com>
2023-05-17 08:06:19 +0545

    Update server.js

--- src/utils/server.js
+++ src/utils/server.js
@@ -1,5 +1,10 @@
-const serverURL = "http://localhost:9000";
-// const serverURL = "https://c882-2602-ffc5-105-302-00-f87-d372.ngrok-free.app";
+// const serverURL = "http://localhost:9000";
+
+//Nile 9000
+// const serverURL = "https://f036-2602-ffc5-105-302-00-f87-d372.ngrok-free.app";
+
+//Mainnet 9001
+const serverURL = "https://b7c0-2602-ffc5-105-302-00-f87-d372.ngrok-free.app";
 
 
 export default serverURL;
